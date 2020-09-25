fef1fb78b78aa6c8c86ad7c11a36775a76a14359
Олег Максимов <gruz103@gmail.com>
2020-09-25 19:02:51 +0300

    sample added

---
+++ Assets/Assets/InputTester.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputTester : MonoBehaviour
+{
+    [SerializeField]
+    InputActionAsset _playerInput;
+    private InputAction _move;
+
+    void Start()
+    {
+        _move = _playerInput.FindActionMap("Player").FindAction("Move");
+        _move.performed += HandleMove;
+    }
+
+    void OnDestroy()
+    {
+        _move.performed -= HandleMove;
+    }
+
+    private void HandleMove(InputAction.CallbackContext callbackContext)
+    {
+        var moveVector = callbackContext.ReadValue<Vector2>();
+        Debug.Log("move invoked: " + moveVector);
+    }
+}
